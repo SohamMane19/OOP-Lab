f61e032516e97b5ef23c9335bb772bb2dad52c06
soham <soham@soham-ASUS-TUF-Gaming-F15-FX506HF-FX506HF>
2023-10-17 11:49:37 +0530

    added assignment 4

--- assignment4.java
+++ assignment4.java
@@ -1,4 +1,67 @@
+import java.util.*;
+
 class assignment4
 {
-	public
+	static void findlen(String val)
+	{
+		System.out.println(val.length());
+	}
+	public static void main(String[] args)
+	{
+		boolean flag=true;
+		int choice;
+		Scanner sc=new Scanner(System.in);
+		while(flag)
+		{
+			System.out.println("1)Divide number by zero exception\n2)Array index out of bounds\n3)Null pointer exception\nEnter your choice:");
+			choice=sc.nextInt();
+			if(choice==1)
+			{
+				int a;
+				int result;
+				System.out.println("enter a number:");
+				a=sc.nextInt();
+				try
+				{
+					result=a/0;
+					System.out.println(result);
+				}
+				catch(Exception e)
+				{
+					System.out.println("You shouldn’t divide a number by zero.");
+				}
+			}
+			else if(choice==2)
+			{
+				int arr[]={1,2,3,4,5,6};
+				System.out.println("Accessing element at index 7:");
+				try
+				{
+					System.out.println(arr[7]);
+				}
+				catch(Exception e)
+				{
+					System.out.println("OOPs!!!Array Index 7 out of bounds for length 6.");
+				}
+			}
+
+			else if(choice==3)
+			{
+				String str=null;
+				try
+				{
+					findlen(str);
+				}
+				catch(Exception e)
+				{
+					System.out.println("Null Pointer Exception arises!!");
+				}
+			}
+			else
+			{
+				System.out.println("exited!");
+				flag=false;
+			}				
+		}
+	}
 }
---
+++ testing_2.java
@@ -0,0 +1,12 @@
+class testing_2
+{
+	static void findlen(String val)
+	{
+		System.out.println(val.length());
+	}
+	public static void main(String args[])
+	{
+		String a=null;
+		findlen(a);
+	}
+}
